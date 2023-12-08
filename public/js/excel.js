1d0a9bf99f8fc493f9d92811c892771fa7ac4589
sultonbayevogabek <sultonbayevogabek@gmail.com>
2023-12-08 15:45:39 +0500

    commit

---
+++ public/js/excel.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let user = JSON.parse(localStorage.getItem('user'));
+
+if (user && user?.name && user?.phone && user?.time) {
+    const formData = new FormData();
+    formData.append('Ismi', user?.name);
+    formData.append('Telefon raqami', user?.phone);
+    formData.append(`Ro'yxatdan o'tgan vaqti`, user?.time);
+
+    fetch('https://script.google.com/macros/s/AKfycbyDapVlvrBKVngMeVairUIVbxbnFJkxvKqr0MV53G-2ugkTfhr2-7UOS0X8bIRwFD-Y/exec', {
+        method: 'POST',
+        body: formData
+    })
+        .then(res => res.json())
+        .then(() => {
+            localStorage.clear();
+        })
+} else {
+    localStorage.clear();
+}
---
+++ public/js/script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+
+// Index page
+try {
+    const registerButton = document.querySelector('.webinar-main-button');
+    const modalBackdrop = document.querySelector('.modal-backdrop');
+    const modalCloserElements = document.querySelectorAll('[data-modal-close]');
+    const form = document.querySelector('.form');
+
+    registerButton.addEventListener('click', () => {
+        modalBackdrop.classList.add('modal-backdrop--open')
+    })
+
+    function closeModal() {
+        modalBackdrop.classList.remove('modal-backdrop--open');
+    }
+
+    window.addEventListener('keydown', e => {
+        if (e.key === 'Escape') {
+            closeModal();
+        }
+    })
+
+    modalCloserElements.forEach(el => {
+        el.addEventListener('click', e => {
+            if (e.target.hasAttribute('data-modal-close')) {
+                closeModal();
+            }
+        })
+    })
+
+    form.addEventListener('submit', async e => {
+        e.preventDefault();
+
+        const submitButton = e.target.querySelector('.form-button');
+        const name = e.target.querySelector('#name').value.trim();
+        const phone = e.target.querySelector('#phone').value?.replace(/[^0-9]/g, '');
+
+        if (name.length && phone?.length === 12) {
+            submitButton.setAttribute('disabled', true);
+            submitButton.textContent = 'Yuborilmoqda...'
+
+            localStorage.setItem('user', JSON.stringify({
+                name,
+                phone,
+                time: new Date().toLocaleString()
+            }))
+
+            window.location.href = `${window.location.protocol}//${window.location.host}/telegram.html`
+        }
+    })
+} catch (e) {
+}
